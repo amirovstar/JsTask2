4940f35016815306e6d66d203d441be912ddef31
amirovstar <furkanza@code.edu.az>
2023-06-06 08:30:14 +0400

    done

---
+++ task/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+function passwordToggle(){
+    let inp=document.getElementById('input');
+    let icon=document.getElementById('eye');
+
+    if(inp.type==='password'){
+        inp.type='text';
+        icon.className=('fa-solid fa-eye-slash');
+    }else{
+        inp.type='password';
+        icon.className=('fa-solid fa-eye');
+    }
+}
+
+
+
+
+const textarea = document.getElementById("inputText");
+
+  
+  function countWords() {
+    const text = textarea.value;
+    let wordCount = 0;
+    let isWord = false;
+
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] !== ' ' && text[i] !== '\n' && text[i] !== '\t') {
+        if (!isWord) {
+          wordCount++;
+          isWord = true;
+        }
+      } else {
+        isWord = false;
+      }
+    }
+
+    return wordCount;
+  }
+
+  textarea.addEventListener("input", function() {
+    const wordCount = countWords();
+    console.log("Word count:", wordCount);
+  });
